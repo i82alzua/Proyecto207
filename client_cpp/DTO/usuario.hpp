ea0035413a36dac44e8f1b2db8b10243da62486f
i82carot <i82carot@uco.es>
2022-12-13 00:27:27 +0100

    commit3

---
+++ client_cpp/DTO/usuario.hpp
@@ -0,0 +1,34 @@
+#ifndef USUARIO_HPP
+#define USUARIO_HPP
+
+#include <string>
+#include "enumUsuarios.hpp"
+
+class Usuario {
+
+    public:
+
+        Usuario();
+        Usuario(const std::string& usuario, const std::string& contrasena, const std::string& dni, TIPO_USUARIO tipo);
+        
+        void setUsuario(const std::string& usuario);
+        std::string getUsuario();
+        
+        void setContasena(const std::string& contrasena);
+        std::string getContrasena();
+        
+        void setTipo(TIPO_USUARIO tipo);
+        int getTipo();
+
+        void setDNI(const std::string& dni);
+        std::string getDNI();
+
+    private:
+        std::string usuario;
+        std::string contrasena;
+        std::string dni;
+        TIPO_USUARIO tipo;
+
+};
+
+#endif
